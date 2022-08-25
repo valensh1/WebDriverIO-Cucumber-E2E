eddd1e65b80fcb476448358967d912b38efb05ea
Shaun Valentine <valensh1@yahoo.com>
2022-08-24 23:01:54 -0700

    Tables Part 2

--- test/features/demo/Tables.feature
+++ test/features/demo/Tables.feature
@@ -3,5 +3,8 @@ Feature: Tables
     @demo
     Scenario Outline: Tables Demo
     Given Navigate to tables webpage
-    Then Check number of tables and rows
+    Then Check number of rows and columns
+    Then Get single row based on a condition
+    Then Get single due amount column
+    Then Get first names of people who owe more than $50
    
--- test/features/step-definitions/then.ts
+++ test/features/step-definitions/then.ts
@@ -30,7 +30,7 @@ Then(/^Validate all products have valid price$/, async function () {
 });
 
 //? Tables.Feature
-Then(/^Check number of tables and rows$/, async function () {
+Then(/^Check number of rows and columns$/, async function () {
   const tableRowsCount = await $$(`table#table1 > tbody > tr`).length;
   console.log(`table rows count = ${tableRowsCount}`);
   chai.expect(tableRowsCount).to.equal(4);
@@ -43,7 +43,7 @@ Then(/^Check number of tables and rows$/, async function () {
   //*[@id="table1"]/tbody/tr[1]/td[4]
   const arr = [];
   for (let i = 1; i <= tableRowsCount; i++) {
-      let obj = {};
+    let obj = {};
     for (let j = 1; j <= columnCount; j++) {
       const cellValue = await $(
         `//table[@id="table1"]/tbody/tr[${i}]/td[${j}]`
@@ -57,4 +57,74 @@ Then(/^Check number of tables and rows$/, async function () {
     arr.push(obj);
   }
   console.log(`The array is ${JSON.stringify(arr)}`);
+});
+
+// Get single row based on condition
+Then(/^Get single row based on a condition$/, async function () {
+  const tableRowsCount = await $$(`table#table1 > tbody > tr`).length; // Create variable to get # of rows in table
+  const columnCount = (await $$(`table#table1 > thead > tr > th`).length) - 1; // Create variable to get # of columns in table less the Action column
+  const arr = []; // Create an empty array to push to
+
+  // Loop through rows under the First Name column which is column #2 and look for name of Jason
+  for (let i = 1; i <= tableRowsCount; i++) {
+    const firstNameCondition = 'Jason';
+    const cellValue = await $(
+      `//table[@id="table1"]/tbody/tr[${i}]/td[2]`
+    ).getText();
+
+    // If rows under First Name column being looped over equal Jason then create an empty object of which we will create key:value pairs for the entire row of the row having first name of Jason
+    if (cellValue === firstNameCondition) {
+      const obj = {}; // Create empty object to push key:value pairs to
+      const rowValues = await $$(`//table[@id="table1"]/tbody/tr[${i}]/td`); // Create variable which will contain the entire row for which contains the firstName of Jason
+      console.log(
+        `ROW VALUES TO LOOP OVER ARE >>> ${JSON.stringify(rowValues)}`
+      );
+
+      // Loop over row containing First Name of Jason and depending on what column it is create the key:value pair to push to the object
+      for (let j = 1; j < columnCount; j++) {
+        const text = await rowValues[j].getText(); // Get the text from each column in the row containing the First Name of Jason
+        console.log(`Row values are >>>>${text}`);
+        if (j === 1) obj.firstName = text;
+        if (j === 2) obj.lastName = text;
+        if (j === 3) obj.email = text;
+        if (j === 4) obj.due = text;
+        if (j === 5) obj.website = text;
+      }
+      arr.push(obj); // Push object to empty array created above
+    }
+  }
+  console.log(`This is the final row >> ${JSON.stringify(arr)}`); // console.log in Cucumber WDIO doesn't print out objects unless you stringify them
+});
+
+Then(/^Get single due amount column$/, async function () {
+  const tableRowsCount = await $$(`table#table1 > tbody > tr`).length; // Create variable to get # of rows in table
+  const arr = [];
+  for (let i = 1; i <= tableRowsCount; i++) {
+    const column = await $(
+      `table#table1 tbody tr:nth-child(${i}) td:nth-child(4)`
+    );
+    const rowValue = await column.getText();
+    const convertedStringToNumber = +parseInt(rowValue.replace('$', ''));
+    arr.push(convertedStringToNumber);
+  }
+  console.log(`This is the due amounts >>> ${arr}`);
+});
+
+Then(/^Get first names of people who owe more than \$50$/, async function () {
+  const tableRowsCount = await $$(`table#table1 > tbody > tr`).length; // Create variable to get # of rows in table
+  const arr = [];
+  for (let i = 1; i <= tableRowsCount; i++) {
+    const column = await $(
+      `table#table1 tbody tr:nth-child(${i}) td:nth-child(4)`
+    ).getText();
+    const convertTextToNumber = parseInt(column.replace('$', ''));
+    if (convertTextToNumber > 50) {
+      const firstName = await $(
+        `table#table1 tbody tr:nth-child(${i}) td:nth-child(2)`
+      ).getText();
+      arr.push(firstName);
+    }
+  }
+  console.log(`This is the final first name array of people who owe more than $50 >>> ${JSON.stringify(arr)}`); // Prints ["Frank", "Jason"]
+  console.log(`This is the final first name array of people who owe more than $50 >>> ${arr}`); // Prints Frank, Jason
 });
