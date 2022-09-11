2b68d4a561e2125efda4100b68fd67d5739c7f1a
Shaun Valentine <valensh1@yahoo.com>
2022-09-11 09:55:34 -0700

    Page Object Model - Part 2

--- test/page-objects/sause.home.page.ts
+++ test/page-objects/sause.home.page.ts
@@ -38,18 +38,42 @@ class HomePage extends Page {
   }
 
   async enterPassword(testid: string, password: string) {
-    if (!userName) throw Error(`Given username: ${userName} is not valid`);
+    if (!password) throw Error(`Given username is not valid`);
     try {
-      userName = userName.trim();
-      await this.typeInto(await this.userNameInputBox, userName);
+      password = password.trim();
+      await this.typeInto(await this.passwordInputBox, password);
       reporter.addStep(
         testid,
         'info',
-        `UserName: ${userName} entered successfully `
+        `password entered successfully `
       );
     } catch (err) {
-      err.message = `Error entering username: ${userName}, ${err.message}`;
+      err.message = `Error entering password, ${err.message}`;
       throw err;
+    }
+  }
+
+  async clickLoginBtn(testid: string) {
+    try {
+      await this.click(await this.loginBtn)
+      reporter.addStep(
+        testid,
+        'info',
+        `Login button clicked `
+      );
+    } catch (err) {
+      err.message = `Error clicking login button, ${err.message}`;
+      throw err;
+    }
+  }
+
+  async loginToSauseApp(testid: string, userName: string, password: string) {
+    try {
+        await this.enterUserName(testid, userName);
+        await this.enterPassword(testid, password);
+        await this.clickLoginBtn(testid);
+    } catch (err) {
+        throw err
     }
   }
 
