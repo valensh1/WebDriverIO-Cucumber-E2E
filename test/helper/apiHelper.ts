3047ef71d8dc0bf1c1d452cc99ceb9f161e5d014
Shaun Valentine <valensh1@yahoo.com>
2022-09-12 23:12:44 -0700

    API Calls

---
+++ test/helper/apiHelper.ts
@@ -0,0 +1,69 @@
+import request from 'supertest';
+import reporter from '../helper/reporter';
+
+const payload = {
+  email: 'eve.holt@reqres.in',
+  password: 'pistol',
+};
+
+async function GET(
+  testid: string,
+  baseURL: string,
+  endpoint: string,
+  authToken: string,
+  queryParam: object
+) {
+  if (!baseURL || !endpoint) {
+    throw Error(
+      `One of the given values baseURL: ${baseURL}, endpoint: ${endpoint} is NOT valid`
+    );
+  }
+  baseURL = baseURL.trim();
+  endpoint = endpoint.trim();
+  reporter.addStep(testid, 'info', `Making a GET to ${endpoint}`);
+  try {
+    return await request(baseURL)
+      .get(endpoint)
+      .query({ queryParam })
+      .auth(authToken, { type: `bearer` })
+      .set('Content-Type', 'application/json')
+      .set('Accept', 'application/json');
+  } catch (err) {
+    err.message = `Error making a GET call to ${endpoint}, ${err}`;
+    throw err;
+  }
+}
+
+async function POST(
+  testid: string,
+  baseURL: string,
+  endpoint: string,
+  authToken: string,
+  payload: object
+) {
+  if (!baseURL || !endpoint) {
+    throw Error(
+      `One of the given values baseURL: ${baseURL}, endpoint: ${endpoint} is NOT valid`
+    );
+  }
+  baseURL = baseURL.trim();
+  endpoint = endpoint.trim();
+  reporter.addStep(testid, 'info', `Making a POST to ${endpoint}`);
+  try {
+    return await request(baseURL)
+      .post(endpoint)
+      .auth(authToken, { type: `bearer` })
+      .set('Content-Type', 'application/json')
+      .set('Accept', 'application/json')
+      .send(payload);
+  } catch (err) {
+    err.message = `Error making a POST call to ${endpoint}, ${err}`;
+    throw err;
+  }
+}
+
+export default { GET, POST };
+
+// BaseURL -> https://reqres.in/
+// Endpoint -> api/users?page=2
+// Query Params -> {pages:2}
